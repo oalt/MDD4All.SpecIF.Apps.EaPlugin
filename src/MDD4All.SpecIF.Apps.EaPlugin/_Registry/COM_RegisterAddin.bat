c10e747a3b158f0cddfff2fc4af1df039c00fdfa
Oliver Alt <oa@lacheweg21.de>
2022-01-10 13:29:51 +0100

    Fixing batch scrips for COM registration

--- src/MDD4All.SpecIF.Apps.EaPlugin/_Registry/COM_RegisterAddin.bat
+++ src/MDD4All.SpecIF.Apps.EaPlugin/_Registry/COM_RegisterAddin.bat
@@ -1,2 +1,2 @@
 @echo off
-c:\Windows\Microsoft.NET\Framework\v4.0.30319\regasm.exe MDD4All.SpecIF.Apps.EaPlugin /codebase
+c:\Windows\Microsoft.NET\Framework\v4.0.30319\regasm.exe MDD4All.SpecIF.Apps.EaPlugin.dll /codebase
--- src/MDD4All.SpecIF.Apps.EaPlugin/_Registry/COM_UnregisterAddin.bat
+++ src/MDD4All.SpecIF.Apps.EaPlugin/_Registry/COM_UnregisterAddin.bat
@@ -1,2 +1,2 @@
 @echo off
-c:\Windows\Microsoft.NET\Framework\v4.0.30319\regasm.exe MDD4All.SpecIF.Apps.EaPlugin /unregister
+c:\Windows\Microsoft.NET\Framework\v4.0.30319\regasm.exe MDD4All.SpecIF.Apps.EaPlugin.dll /unregister
